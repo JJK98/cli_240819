f599d9038894b9edcad416a6dd6d04ff9e9712ca
JJk <wnsrb1594@naver.com>
2024-08-19 12:27:10 +0900

    System.ou1-1complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,19 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi!!??.. hey stop");
+        System.out.println("== 게시판 앱 ==");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("명령)");
+            String command = sc. nextLine();
+
+            if (command.equals("종료")){
+                break;
+            }
+        }
     }
 }
