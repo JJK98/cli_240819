5024e30f8870471db37456f803ace1768c1ad182
JJk <wnsrb1594@naver.com>
2024-08-19 12:52:37 +0900

    4lev

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,12 +11,20 @@ public class App {
 
     void run(){
         System.out.println("== 게시판 앱 ==");
+        int count = 1;
         while (true) {
             System.out.println("명령)");
-            String command = sc.nextLine();
-
+            String command = sc.nextLine().trim();
             if (command.equals("종료")) {
                 break;
+            }
+            else if (command.equals("등록")){
+                System.out.println("제목:");
+                String tit = sc.nextLine().trim();
+                System.out.println("내용 : ");
+                String det = sc.nextLine().trim();
+                System.out.println(count + "번 개시물이 출렸되었습니다.");
+                count++;
             }
         }
     }
