3480c9976dc5f2f0570847bdbd96110161d7d605
JJk <wnsrb1594@naver.com>
2024-08-19 13:13:06 +0900

    내풀이

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,8 @@
 package org.example;
 
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.List;
 public class App {
 
     Scanner sc;
@@ -12,20 +13,34 @@ public class App {
     void run(){
         System.out.println("== 게시판 앱 ==");
         int count = 1;
+        List<String> title = new ArrayList<>();
+        List<String> detail = new ArrayList<>();
         while (true) {
-            System.out.println("명령)");
+            System.out.print("명령)");
             String command = sc.nextLine().trim();
             if (command.equals("종료")) {
                 break;
             }
             else if (command.equals("등록")){
-                System.out.println("제목:");
+                System.out.print("제목:");
                 String tit = sc.nextLine().trim();
-                System.out.println("내용 : ");
+                title.add(tit);
+                System.out.print("내용 : ");
                 String det = sc.nextLine().trim();
+                detail.add(det);
                 System.out.println(count + "번 개시물이 출렸되었습니다.");
                 count++;
+            }
+            else if (command.equals("목록")){
+                for(int i = 1; i< count; i++){
+                    System.out.println(i +" / "+title.get(i-1) + " / " +detail.get(i-1));
+                }
             }
         }
     }
 }
+
+
+//class space {
+//
+//}
