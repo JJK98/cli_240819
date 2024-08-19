e7769df672d2f63cacdc598016727ff9e6dd4f96
JJk <wnsrb1594@naver.com>
2024-08-19 12:35:52 +0900

    whdfy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,8 @@ package org.example;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 게시판 앱 ==");
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("명령)");
-            String command = sc. nextLine();
-
-            if (command.equals("종료")){
-                break;
-            }
-        }
+        new App(sc).run();
+        sc.close();
     }
 }
